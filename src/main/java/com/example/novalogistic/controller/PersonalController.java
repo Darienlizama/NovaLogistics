3096cd39782f543dbaf2a38468be1aecbf80d649
GerardoDuoc <gera.silvaa@duocuc.cl>
2026-05-07 02:04:15 -0400

    Clean up PersonalController by removing comments
    Removed commented test code from PersonalController.

--- src/main/java/com/example/novalogistic/controller/PersonalController.java
+++ src/main/java/com/example/novalogistic/controller/PersonalController.java
@@ -1,5 +1,5 @@
 package com.example.novalogistic.controller;
-
+//test esto es un test para poder hacer el codigo, bien, podria dejar aca pegado el codigo pero lo mejor es que se saquen el archivo rar que mande
 public class PersonalController 
 {
     
