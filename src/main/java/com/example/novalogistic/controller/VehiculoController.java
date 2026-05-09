2435e1b5cf6a790115636ef6a01c394fe8442fd1
GerardoDuoc <gera.silvaa@duocuc.cl>
2026-05-09 01:21:34 -0400

    Add files via upload

---
+++ src/main/java/com/example/novalogistic/controller/VehiculoController.java
@@ -0,0 +1,29 @@
+package com.example.novalogistic.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import jakarta.validation.Valid;
+import com.example.novaLogistic.dto.VehiculoDTO;
+import com.example.novaLogistic.service.VehiculoService;
+
+@RestController
+@RequestMapping("/vehiculo")
+
+public class VehiculoController 
+{
+    
+}
